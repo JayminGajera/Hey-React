10abbaf1f90df884d96b2a4235871fa611ae3588
JayminGajera <jaymingajera2332@gmail.com>
2023-11-10 09:38:02 +0530

    episode-01

---
+++ 01-Inception/App.js
@@ -0,0 +1,40 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World From React !"
+);
+//object in above line is used where you will give attribute to your tags.
+//exp. my h1 tag want id of heading so {id:"heading"}
+console.log(heading); //it is the object, it is not h1 tag
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); //render is used to take heading object and put it into heading tag
+
+//how to create nested structure in react
+/*
+    <div id="parent">
+        <div id="child">
+            <h1>I'm h1</h1>
+            <h2>I'm h2</h2>
+        </div>
+        <div id="child2">
+            <h1>I'm h1</h1>
+            <h2>I'm h2</h2>
+        </div>
+    </div>
+*/
+
+//this is the core react
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1"),
+    React.createElement("h2", {}, "I'm h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1"),
+    React.createElement("h2", {}, "I'm h2"),
+  ]),
+]);
+
+//this react code very messay and unreadable that is why JSX is exist
