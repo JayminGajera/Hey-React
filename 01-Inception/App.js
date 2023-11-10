5a90dbd00acf71d933b8d3ba03a1e1ca995f8050
JayminGajera <jaymingajera2332@gmail.com>
2023-11-10 17:51:07 +0530

    Play with code in react

--- 01-Inception/App.js
+++ 01-Inception/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World From React !"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World From React !"
+// );
 //object in above line is used where you will give attribute to your tags.
 //exp. my h1 tag want id of heading so {id:"heading"}
-console.log(heading); //it is the object, it is not h1 tag
+// console.log(heading); //it is the object, it is not h1 tag
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); //render is used to take heading object and put it into heading tag
+// root.render(heading); //render is used to take heading object and put it into heading tag
 
 //how to create nested structure in react
 /*
@@ -30,15 +30,43 @@ root.render(heading); //render is used to take heading object and put it into he
 */
 
 //this is the core react
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1"),
-    React.createElement("h2", {}, "I'm h2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1"),
-    React.createElement("h2", {}, "I'm h2"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm h1"),
+//     React.createElement("h2", {}, "I'm h2"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I'm h1"),
+//     React.createElement("h2", {}, "I'm h2"),
+//   ]),
+// ]);
 
 //this react code very messay and unreadable that is why JSX is exist
+
+//Element
+const jsxHeading = (
+  <h1 id="heading">Hello Everyone - In My React Learning Book</h1>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);//this is how React Element render
+
+const Title = () => (
+  <h2>call the function</h2>
+);
+
+//Component
+const HeadingComponent = () => {
+  return (
+    <>
+      {jsxHeading}
+      {Title()}
+      <Title/>
+      <Title></Title>
+      <h1>This is Component</h1>
+    </>
+  )
+}
+
+root.render(<HeadingComponent/>) //this is how React Component render
