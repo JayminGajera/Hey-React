af64755458ab70fa5f607435fc7d7860f4cc726a
JayminGajera <jaymingajera2332@gmail.com>
2023-12-12 10:14:03 +0530

    api added

--- 02-Food_order_app/src/utils/useRestaurantMenu.js
+++ 02-Food_order_app/src/utils/useRestaurantMenu.js
@@ -10,9 +10,16 @@ const useRestaurantMenu = (resId) => {
 
   const fetchMenu = async () => {
     try {
-      const data = await fetch(
-        `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.246892&lng=72.917604&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
-      );
+      if(window.innerWidth > 600){
+        var data = await fetch(
+          `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.246892&lng=72.917604&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
+        );
+      }else{
+        var data = await fetch(
+          `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.246892&lng=72.917604&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
+        );
+      }
+      
       const json = await data.json();
       console.log("Menu data", json);
       setResInfo(json?.data);
