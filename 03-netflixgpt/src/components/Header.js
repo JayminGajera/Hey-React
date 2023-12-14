282f6561edec3371e4c2d11a7f51a75b344347f2
JayminGajera <jaymingajera2332@gmail.com>
2023-12-14 13:56:16 +0530

    firebase authentication added + redux store created

--- 03-netflixgpt/src/components/Header.js
+++ 03-netflixgpt/src/components/Header.js
@@ -1,11 +1,37 @@
+import {signOut} from "firebase/auth";
+import {auth} from "../utils/firebase";
+import {useNavigate} from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Header = () => {
+
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      navigate("/")
+    }).catch((error) => {
+      navigate("/error");
+    });
+  }
+
   return (
-    <div className="absolute z-10">
+    <div className="absolute w-screen z-10 flex justify-between">
       <img
         className="w-56 px-8 py-2 bg-gradient-to-b from-black"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
+
+      <div className="flex gap-x-1 items-start mr-12">
+          <img
+            src={user?.photoURL}
+            className="w-7 h-7 mt-2"
+          />
+        
+        <button onClick={handleSignOut} className=" mt-2 p-1 border border-white bg-black text-white">Sign out</button>
+      </div>
     </div>
   );
 };
--- 03-netflixgpt/src/components/Login.js
+++ 03-netflixgpt/src/components/Login.js
@@ -1,8 +1,99 @@
 import Header from "./Header";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleBtnClick = () => {
+    //validate the form data
+
+    console.log(name.current.value);
+    console.log(email.current.value);
+    console.log(password.current.value);
+
+    const msg = checkValidData(email.current.value, password.current.value);
+    console.log(msg);
+    setErrorMessage(msg);
+
+    if (msg) return;
+
+    // signIn / signUp
+
+    if (!isSignInForm) {
+      //SignUp logic
+
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/114140596?v=4",
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/Browse");
+            })
+            .catch((error) => setErrorMessage(error.message));
+          console.log("user", user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //SignIn logic
+
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log("user login", user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
+  };
 
   const toggelSignInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -11,35 +102,45 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div className="absolute">
+      <div className="absolute hidden sm:block">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="bg-img"
         />
       </div>
 
-      <form className="absolute text-white bg-black flex flex-col w-96 p-10 mt-28 mx-auto left-0 right-0 rounded-sm bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute text-white bg-black flex flex-col w-96 p-10 mt-28 mx-auto left-0 right-0 rounded-md bg-opacity-80"
+      >
         <h1 className="text-3xl font-semibold mb-7">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
+            ref={name}
             type="text"
             placeholder="Full Name"
-            className="p-4 my-4 bg-gray-700"
+            className="p-4 my-4 bg-gray-700 rounded-md"
           />
         )}
         <input
+          ref={email}
           type="email"
           placeholder="Email Address"
-          className="p-4 my-4 bg-gray-700"
+          className="p-4 my-4 bg-gray-700 rounded-md"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
-          className="p-4 my-4 bg-gray-700"
+          className="p-4 my-4 bg-gray-700 rounded-md"
         />
-        <button className="py-4 my-6 bg-red-700 font-semibold w-full">
+        <p className="text-red-500">{errorMessage}</p>
+        <button
+          className="py-4 my-6 bg-red-700 font-semibold w-full rounded-md"
+          onClick={handleBtnClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="cursor-pointer" onClick={toggelSignInForm}>
